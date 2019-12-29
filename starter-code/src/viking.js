807401be3ddaa65b9dc323fb86cee407afcbc0d6
Juliane Arruda <juliane_ch.arruda@live.com>
2019-12-29 00:55:44 -0300

    saxon stage completed

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -37,7 +37,17 @@ class Viking extends Soldier {
 }
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+
+  receiveDamage(damage) {
+    super.receiveDamage(damage);
+    if (this.health > 0) {
+      return `A Saxon has received ${damage} points of damage`;
+    } else if (this.health === 0) {
+      return `A Saxon has died in combat`;
+    }
+  }
+}
 
 // War
 class War {}
